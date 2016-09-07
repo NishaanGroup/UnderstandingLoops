edd535e37a303a76d285bf301eca9503c7032ba8
kiran paturi <kpaturi@smarttuition.com>
2016-09-07 19:54:37 +0530

    understanding loops

--- UnderstandingLoops/Program.cs
+++ UnderstandingLoops/Program.cs
@@ -10,6 +10,50 @@ namespace UnderstandingLoops
     {
         static void Main(string[] args)
         {
+            //PrintFibonacci(20);
+            //PrintFibonacciRecursive(0, 1, 1, 10);
+            int i = 10;
+            string num = i.ToString();
+
+            string count = "20Mma";
+            int counter;
+            bool isParsed = int.TryParse(count, out counter);
+            if (isParsed)
+            {
+                Console.WriteLine(counter);
+            }
+            else
+            {
+                Console.WriteLine("Cannot convert the string to integer");
+            }
+            Console.ReadKey();
         }
+        static void PrintFibonacci(int count)
+        {
+            int start = 0;
+            int first = 1;
+            
+            for(int i =0; i < count; i++)
+            {
+                int temp = start;//0
+                start = first;//
+                first = temp + start;//
+                //0,1,1,2,3,5,8,13,21...
+                Console.WriteLine(start);
+            }
+            Console.ReadKey();
+        }
+
+        static void PrintFibonacciRecursive(int a, int b,int counter,int length)
+        {
+            if (counter < length)
+            {
+                Console.WriteLine(a);
+                PrintFibonacciRecursive(b, a + b, counter + 1, length);
+            }
+            Console.ReadKey();
+
+        }
+        
     }
 }
